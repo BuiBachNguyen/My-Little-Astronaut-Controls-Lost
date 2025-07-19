3c8d705b066358b29ca2c83f2a66436e58fd0e71
BuiBachNguyen <23521036@gm.uit.edu.vn>
2025-07-19 16:05:28 +0700

    Add: DialogueBox, the Pallet and LAB 1-1

---
+++ Assets/__Object/Manager/Dialouge/DialougeManager.cs
@@ -0,0 +1,91 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine.UI;
+
+public class DialougeManager : MonoBehaviour
+{
+    public Image actorImage;
+    public TextMeshProUGUI actorName;
+    public TextMeshProUGUI messageText;
+    public RectTransform backgroundBox;
+    public GameObject CanvasController;
+    Message[] currentMessages;
+    Actor[] currentActors;
+    int activeMessage = 0;
+
+    public bool isSkip;
+    void Start()
+    {
+        isSkip = false;
+        backgroundBox.transform.localScale = Vector3.zero;
+    }
+    void Update()
+    {
+
+    }
+
+    public void OpenDialogue(Message[] messages, Actor[] actors)
+    {
+        currentMessages = messages;
+        currentActors = actors;
+        activeMessage = 0;
+        CanvasController.SetActive(false);
+        DisplayMessage();
+        backgroundBox.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
+    }
+
+    void DisplayMessage()
+    {
+        Message messageToDisplay = currentMessages[activeMessage];
+        messageText.text = messageToDisplay.message;
+
+        Actor actorToDisplay = currentActors[messageToDisplay.actorID];
+        actorName.text = actorToDisplay.name;
+        actorImage.sprite = actorToDisplay.sprite;
+
+        AnimateTextColor();
+    }
+    
+    public void NextMessage()
+    {
+        activeMessage++;
+        if (activeMessage < currentMessages.Length)
+        {
+            DisplayMessage();
+        }
+        else
+        {
+            isSkip = true;
+        }
+    }
+    public void SkipDialogue()
+    {
+        if (!isSkip)
+        {
+            activeMessage = currentMessages.Length - 1;
+            DisplayMessage();
+            isSkip = true;
+        }
+        else
+        {
+            backgroundBox.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            StartCoroutine("Wait_Coroutine");
+        }
+
+    }
+    public IEnumerator Wait_Coroutine()
+    {
+        yield return new WaitForSeconds(0.6f);
+        backgroundBox.gameObject.SetActive(false);
+        CanvasController.SetActive(true);
+    }
+
+    void AnimateTextColor()
+    {
+        LeanTween.textAlpha(messageText.rectTransform, 0, 0);
+        LeanTween.textAlpha(messageText.rectTransform, 1, 0.5f);
+    }    
+}
---
+++ Assets/__Object/Manager/Dialouge/DialougeTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialougeTrigger : MonoBehaviour
+{
+    public GameObject DialogueManager;
+    public Message[] messages;
+    public Actor[] actors;
+    private bool wasPlayed = false;
+    //public Animator animator;
+    public int WaitTime;
+    //
+    public void StartDialogue()
+    {
+        if (wasPlayed) return;
+        DialogueManager.SetActive(true);
+        FindFirstObjectByType<DialougeManager>().OpenDialogue(messages, actors);
+        wasPlayed = true;
+        //animator.SetBool("isOn", true);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))// && !wasPlayed)
+        {
+            StartCoroutine("Wait_Coroutine");
+        }
+    }
+
+    public IEnumerator Wait_Coroutine()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        StartDialogue();
+    }
+}
+[System.Serializable]
+public class Message
+{
+    public int actorID;
+    public string message;
+}
+[System.Serializable]
+public class Actor
+{
+    public string name;
+    public Sprite sprite;
+}
