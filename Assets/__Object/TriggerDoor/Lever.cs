40a3b97e68d52f407bc13bdf68f26b1aa3a22819
BuiBachNguyen <23521036@gm.uit.edu.vn>
2025-07-21 16:37:29 +0700

    New:LAB 1-3: TriggerDoor

---
+++ Assets/__Object/TriggerDoor/Lever.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] GameObject[] doors;
+    [SerializeField] bool isTriggered = false;
+    [SerializeField] Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag(Tag.Player))
+        {
+            isTriggered = true;
+            animator.SetBool("isActivated", isTriggered);
+            StartCoroutine(Wait());
+        }    
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f);
+        foreach (GameObject go in doors)
+        {
+            go.SetActive(false);
+        }
+    }    
+}
